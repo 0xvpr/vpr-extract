b3490be6723aadf611827c2071985ffaec2b6d5c
0xvpr <malikrb@live.com>
2023-01-01 00:25:37 -0500

    Slight code refactor; Added credits/information in main source file

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,4 +1,14 @@
-#include <filesystem>
+/**
+ * Created by:      VPR
+ * Created:         June 5th, 2022
+ *
+ * Updated by:      VPR
+ * Updated:         January 1st, 2023
+ *
+ * Description:     A tool for extracting and dumping the .text section
+ *                  of a COFF object file.
+**/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -6,49 +16,50 @@
 
 #include <cstring>
 
-using io_t = std::pair<std::string, std::string>;
-
 constexpr const char * target_section = ".text";
 
 namespace machine_type {
-    constexpr unsigned x86 = 0x014C;
-    constexpr unsigned iit = 0x0200;
-    constexpr unsigned x64 = 0x8664;
+    constexpr unsigned x86          = 0x014C;
+    constexpr unsigned iit          = 0x0200;
+    constexpr unsigned x64          = 0x8664;
 }
 
 struct IMAGE_FILE_HEADER {
-  uint16_t   Machine              : 16;
-  uint16_t   NumberOfSections     : 16;
-  uint32_t   TimeDateStamp        : 32;
-  uint32_t   PointerToSymbolTable : 32;
-  uint32_t   NumberOfSymbols      : 32;
-  uint16_t   SizeOfOptionalHeader : 16;
-  uint16_t   Characteristics      : 16;
+    uint16_t   Machine              : 16;
+    uint16_t   NumberOfSections     : 16;
+    uint32_t   TimeDateStamp        : 32;
+    uint32_t   PointerToSymbolTable : 32;
+    uint32_t   NumberOfSymbols      : 32;
+    uint16_t   SizeOfOptionalHeader : 16;
+    uint16_t   Characteristics      : 16;
 };
 
 struct IMAGE_SECTION_HEADER {
-  unsigned char  Name[8];
-  union {
-    uint32_t PhysicalAddress      : 32;
-    uint32_t VirtualSize          : 32;
-  } Misc;
-  uint32_t   VirtualAddress       : 32;
-  uint32_t   SizeOfRawData        : 32;
-  uint32_t   PointerToRawData     : 32;
-  uint32_t   PointerToRelocations : 32;
-  uint32_t   PointerToLinenumbers : 32;
-  uint16_t   NumberOfRelocations  : 16;
-  uint16_t   NumberOfLinenumbers  : 16;
-  uint32_t   Characteristics      : 32;
+    unsigned char  Name[8];
+    union {
+        uint32_t PhysicalAddress    : 32;
+        uint32_t VirtualSize        : 32;
+    } Misc;
+    uint32_t   VirtualAddress       : 32;
+    uint32_t   SizeOfRawData        : 32;
+    uint32_t   PointerToRawData     : 32;
+    uint32_t   PointerToRelocations : 32;
+    uint32_t   PointerToLinenumbers : 32;
+    uint16_t   NumberOfRelocations  : 16;
+    uint16_t   NumberOfLinenumbers  : 16;
+    uint32_t   Characteristics      : 32;
 };
 
 static inline void __attribute__((noreturn)) __usage_error(const char* msg, int exit_code) {
+
     std::cerr << "Usage error: " << msg << "\n";
 
     exit(exit_code);
+
 }
 
 int main(int argc, char** argv) {
+
     if (argc < 2) {
         __usage_error("No file specified.", 1);
     } else if (argc > 256) {
@@ -57,15 +68,19 @@ int main(int argc, char** argv) {
 
     --argc;
     ++argv;
-    std::vector<io_t> targets((argc) & 0xFF);
+
+    using io_pair_t = std::pair<std::string, std::string>;
+    std::vector<io_pair_t> targets(argc & 0xFF);
+
     for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << " " << "out.bin" << "\n";
-        targets[i & 0xFF].first  = argv[i];
-        targets[i & 0xFF].second = "out-" + std::to_string(i) + ".bin";
+        auto& target = targets.at(i & 0xFF);
+        target.first  = argv[i];
+        target.second = "out-" + std::to_string(i) + ".bin";
+        std::cout << target.first << " " << target.second << "\n";
     }
 
     for (const auto& target : targets) {
-        std::cout << "Target:\t" << target.first << " -> " << target.second << "\n"; // TODO dbg
+        std::cout << "Target:\t" << target.first << " -> " << target.second << "\n";
         std::ifstream file(target.first, std::ios::binary);
         if (!file.is_open()) {
             __usage_error("File could not be opened.", 2);
@@ -105,14 +120,15 @@ int main(int argc, char** argv) {
         file.read(reinterpret_cast<char *>(data.data()), text_section->SizeOfRawData);
         file.close();
 
-        std::cout << "Size of " << target.first << ":\t" << data.size() << "\n"; // TODO: dbg
+        std::cout << "Size of " << target.first << ":\t" << data.size() << "\n";
 
         std::ofstream outfile(target.second, std::ios::binary);
         outfile.write(reinterpret_cast<char *>(data.data()), text_section->SizeOfRawData);
         outfile.close();
 
-        std::cout << "written to '" << target.second << "'.\n";
+        std::cout << "Written to '" << target.second << "'.\n";
     }
 
     return 0;
+
 }
